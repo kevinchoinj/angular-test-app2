0be88b9f6906adfe0a3577d35d5ac052e05e7987
kevinchoinj <kevinchoinj@yahoo.com>
2023-02-08 17:42:36 -0600

    angular udemy update; httpmodule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-pg3';
+  name = "Max"
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TextRowComponent } from './text-row/text-row.component';
 import { TestPageComponent } from './test-page/test-page.component';
 import { RemoteAssociatesTestComponent } from './remote-associates-test/remote-associates-test.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
@@ -14,8 +16,9 @@ import { RemoteAssociatesTestComponent } from './remote-associates-test/remote-a
     TextRowComponent,
     TestPageComponent,
     RemoteAssociatesTestComponent,
+    ServerComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
